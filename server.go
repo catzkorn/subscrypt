2172ba0406975f5140523d9bffcaa397ad8b9706
farhaan-ali <fa8g12@gmail.com>
2020-11-10 20:07:45 +0000

    Added function definitions

--- server.go
+++ server.go
@@ -5,19 +5,17 @@ import (
 	"net/http"
 )
 
+// NewSubscriptionServer returns a instance of a SubscriptionServer
 func NewSubscriptionServer(store SubscriptionStore) *SubscriptionServer {
 	s := new(SubscriptionServer)
-
 	s.store = store
-
 	router := http.NewServeMux()
 	router.Handle("/", http.HandlerFunc(s.subscriptionHandler))
-
 	s.Handler = router
-
 	return s
 }
 
+// subscriptionHandler handles the routing logic for the index
 func (s *SubscriptionServer) subscriptionHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -34,17 +32,19 @@ func (s *SubscriptionServer) subscriptionHandler(w http.ResponseWriter, r *http.
 	}
 }
 
-
+// SubscriptionServer is the HTTP interface for subscription information
 type SubscriptionServer struct {
 	store SubscriptionStore
 	http.Handler
 }
 
+// SubscriptionStore stores information about individual subscriptions
 type SubscriptionStore interface {
 	GetSubscriptions() []Subscription
 	RecordSubscription(subscription Subscription)
 }
 
+// Subscription stores the id, name, amount and datedue of an individual subscription
 type Subscription struct {
 	Id int
 	Name string
