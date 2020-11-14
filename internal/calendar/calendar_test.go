f0673baa5f12bdb793bc9a44fe27ede4c3fdbdcf
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-11-14 14:40:27 +0000

    WIP Test for calendar invite creation
    - Work on determining most sustainable test

--- internal/calendar/calendar_test.go
+++ internal/calendar/calendar_test.go
@@ -1,17 +1,50 @@
 package calendar
 
 import (
+	"os"
 	"testing"
 	"time"
+
+	"github.com/Catzkorn/subscrypt/internal/reminder"
+	"github.com/Catzkorn/subscrypt/internal/subscription"
+	ics "github.com/arran4/golang-ical"
+	"github.com/shopspring/decimal"
 )
 
 func TestCalendarInviteCreation(t *testing.T) {
 
-	t.Run("checks that a .ics file is containg the correct information", func(t *testing.T) {
+	amount, _ := decimal.NewFromString("8.00")
+	subscription := subscription.Subscription{
+		ID:      1,
+		Name:    "Netflix",
+		Amount:  amount,
+		DateDue: time.Date(2020, time.November, 16, 0, 0, 0, 0, time.UTC),
+	}
 
-		_ = CreateReminderInvite(12, time.Date(2020, time.November, 11, 0, 0, 0, 0, time.UTC), time.Date(2020, time.November, 16, 0, 0, 0, 0, time.UTC), "Netflix", "gary@gopher.com")
+	reminder := reminder.Reminder{
+		Email:          os.Getenv("EMAIL"),
+		SubscriptionID: 1,
+		ReminderDate:   time.Date(2020, time.November, 11, 0, 0, 0, 0, time.UTC),
+	}
 
-		// ioutil.WriteFile("./calendartest.ics", []byte(cal), 0644)
+	t.Run("checks that a .ics file is containing the correct information", func(t *testing.T) {
+
+		cal := CreateReminderInvite(subscription, reminder)
+
+		t.Log(len(cal.Components))
+		for _, component := range cal.Components {
+
+			switch c := component.(type) {
+			case *ics.VEvent:
+				t.Log(c)
+			default:
+				t.Errorf("did not create a VEvent")
+			}
+		}
+
+		// calfile := strings.NewReader(cal.Serialize())
+
+		// _, err := ics.ParseCalendar(calfile)
 
 	})
 
