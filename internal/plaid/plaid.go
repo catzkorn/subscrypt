3414f7e10bcbc4f7903bbd5ee0516ab8d7d1f31f
farhaan-ali <fa8g12@gmail.com>
2020-11-17 09:15:44 +0000

    Refactor getTransaction interface method on Plaid API

--- internal/plaid/plaid.go
+++ internal/plaid/plaid.go
@@ -3,7 +3,6 @@ package plaid
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -55,17 +54,18 @@ type Transaction struct {
 	Name         string `json:"name"`
 }
 
-func (p *PlaidAPI) GetTransactions() {
+func (p *PlaidAPI) GetTransactions() (TransactionList, error){
 	response := getPublicToken()
 	access := getAccessToken(response)
 	time.Sleep(10 * time.Second)
-	getTransactions(access)
+	TransactionList := getTransactions(access)
+
+	return TransactionList , nil
 }
 
 func getPublicToken() PublicToken {
 	m := GetPublicToken{ClientId: os.Getenv("CLIENT_ID"), Secret: os.Getenv("SECRET"), InstitutionId: "ins_3", InitialProducts: []string{"auth","transactions"},Options: map[string]string{"webhook": "https://www.genericwebhookurl.com/webhook"} }
 	b, _ := json.Marshal(m)
-	fmt.Println(string(b))
 	r , _ := http.Post("https://sandbox.plaid.com/sandbox/public_token/create", "application/json", bytes.NewBuffer(b))
 	data, _ := ioutil.ReadAll(r.Body)
 	var response PublicToken
@@ -95,12 +95,11 @@ func getAccessToken(response PublicToken) AccessToken{
 	return access
 }
 
-func getTransactions(access AccessToken) {
+func getTransactions(access AccessToken) TransactionList {
 	t := GetTransactions{ClientId: os.Getenv("CLIENT_ID"), Secret: os.Getenv("SECRET"), AccessToken: access.Token, StartDate: "2018-11-10", EndDate: "2020-11-10"}
 	b, _ := json.Marshal(t)
 	r, _ := http.Post("https://sandbox.plaid.com/transactions/get", "application/json", bytes.NewBuffer(b))
 	data, _ := ioutil.ReadAll(r.Body)
-	fmt.Printf("%s\n", data)
 	var listOfTransactions TransactionList
 
 	err := json.Unmarshal(data, &listOfTransactions)
@@ -108,5 +107,5 @@ func getTransactions(access AccessToken) {
 		println(err)
 	}
 
-	fmt.Println(listOfTransactions)
+	return listOfTransactions
 }
